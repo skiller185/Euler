f31c8e7ff9ad835f682be08834cf0684e949ef2c
Anthony Carpenetti <31355951+skiller185@users.noreply.github.com>
2017-10-24 23:42:23 -0400

    Add files via upload

--- Euler2/Euler2.java
+++ Euler2/Euler2.java
@@ -1,1 +1,24 @@
+//Anthony Carpenetti
+//When executed, Euler2 prints the sum of all even fibonacci numbers less than 4000000
 
+public class Euler2
+{
+   public static void main(String[] args)
+   {
+      int ans = 0;
+      int a = 0;
+      int b = 1;
+      int c = 0;
+      while(a < 4000000)
+      {
+         if(a % 2 == 0)
+         {
+            ans += a;
+         }
+         c = b;
+         b += a;
+         a = c;
+      }
+      System.out.println(ans);
+   }
+}
