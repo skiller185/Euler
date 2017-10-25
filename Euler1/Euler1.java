776f2e1a2c54182e42fbf64b11e49e5805e10b4a
Anthony Carpenetti <31355951+skiller185@users.noreply.github.com>
2017-10-24 23:21:42 -0400

    Add files via upload

--- Euler1/Euler1.java
+++ Euler1/Euler1.java
@@ -1,1 +1,18 @@
+//Anthony Carpenetti
+//When executed, Euler1 prints the sum of all numbers divisible by 3 or 5 less than 1000
 
+public class Euler1
+{
+	public static void main(String[] args)
+	{
+      int ans = 0;
+		for(int i = 1;i < 1000;i++)
+      {
+         if(i % 3 == 0 || i % 5 == 0)
+         {
+            ans += i;
+         }
+      }
+      System.out.println(ans);
+	}
+}
