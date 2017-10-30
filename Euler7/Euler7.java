4d3a17a2f4666ff49dea2cac34aad70558e718b4
Anthony Carpenetti <31355951+skiller185@users.noreply.github.com>
2017-10-30 08:50:58 -0400

    Add files via upload

--- Euler7/Euler7.java
+++ Euler7/Euler7.java
@@ -1,1 +1,33 @@
+//Anthony Carpenetti
+//When executed, Euler7...
 
+public class Euler7
+{
+	public static void main(String[] args)
+	{
+		int lastPrime = 3;
+		for(int i = 1;i < 10001;i++)
+		{
+			
+		}
+	}
+	
+	public static boolean isPrime(int n) //returns if n is prime
+	{
+		if(n <= 1)
+		{
+			return false;
+		}
+		else
+		{
+			for(int i = 2;i < n;i++)
+			{
+				if(n % i == 0)
+				{
+					return false;
+				}
+			}
+			return true;
+		}
+	}
+}
